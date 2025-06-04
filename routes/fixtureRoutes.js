e9712926119d15395dd76026a143f364e01ee752
lilmeezyd <33873816+lilmeezyd@users.noreply.github.com>
2025-06-04 22:35:37 +0000

    initial commit

--- routes/fixtureRoutes.js
+++ routes/fixtureRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express';
 const router = express.Router();
-import { createFixture } from '../controllers/fixtureController.js';
+import { createFixture, scoreFixtureById } from '../controllers/fixtureController.js';
 import { protect } from '../middleware/authMiddleware.js';
-import scoreFixtureById from "../controllers/scoreFixtureById";
+
 
 router.post('/', protect, createFixture);
 
