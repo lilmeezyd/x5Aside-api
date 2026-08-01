6465f241bfd3daf81ba75918faa1afc7562143c8
Denis Moini <denismoini09@gmail.com>
2026-08-02 00:04:26 +0300

    edited teamModel

--- models/teamModel.js
+++ models/teamModel.js
@@ -4,15 +4,15 @@ const teamSchema = new mongoose.Schema({
   id: Number,
   name: String,
   short_name: String,
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: ""},
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", default: null},
   url: { type: String, default:"" },
   fileId: { type: String, default: "" },
   fileName: { type: String, default: "" },
-  players: [{ type:  mongoose.Schema.Types.ObjectId, ref: "Player",
+  players: [{ type:  mongoose.Schema.Types.ObjectId, ref: "Player"}],
   primaryColor: { type: String, default: ""},
   secondaryColor: { type: String, default: ""},
   color: { type: String, default: ""}   
-}]}, { timestamps: true });
+}, { timestamps: true });
 /*const Team = mongoose.model("Team", teamSchema);
 */
 export default teamSchema;
